2b8c38f22d89758dc151dde039309fe8017b3ead
unknown <alessio.romitelli@itssmartacademy.it>
2025-01-31 17:54:41 +0100

    first commit

---
+++ login.js
@@ -0,0 +1,9 @@
+function controllo() {
+    let email = document.getElementById("username").value;
+    let password = document.getElementById("password").value;
+    if (email == "alessio.itssmartacademy.it" && password == "1234") {
+        console.log("giusto")
+    } else {
+        console.log("aaaaaaaaaaaaaaaaa")
+    }
+}
